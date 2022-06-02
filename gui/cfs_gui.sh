2d144f1b654fcd8545c5252ab2d7d7623f3fadd9
Soul Inferno DE <SoulInfernoDE@users.noreply.github.com>
2022-06-02 12:19:16 +0200

    Kernel v.5.18+ ASHMEM fix
    K.ver.15.8+ has dropped ASHMEM completly which makes Anbox unusable. This quick commit fixes it for now for the "user" part of anbox. MEMFD is the successor to ASHMEM which is not supported by Anbox yet - it is until now unclear if it ever will... This fix may break any time with further updates...

--- gui/cfs_gui.sh
+++ gui/cfs_gui.sh
@@ -17,6 +17,17 @@ wget 'https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-'$KERNEL_VERSION'.tar.x
 tar xvf linux-* -C kernel/ --strip-components=1
 cd kernel
 cp /boot/config-$(uname -r) ./.config
+echo 'Downloading the ASHMEM source code removal patch from upstream'
+wget -O remove_ashmem.patch https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/patch/?id=721412ed3d819e767cac2b06646bf03aa158aaec
+echo ''
+echo 'Reverting the removal patch code..'
+interdiff -q remove_ashmem.patch /dev/null > enable_ashmem.patch
+echo ''
+echo 'Patching the kernel sources to bring back ASHMEM..'
+patch -p1 -i enable_ashmem.patch
+echo ''
+echo 'DONE! ASHMEM is now selectable again in your kernel .config file! NOTE: THIS MAY BREAK ANYTIME AS ASHMEM IS REPLACED WITH MEMFD'
+echo 'which is not supported by Anbox yet..'
 make olddefconfig
 echo 'Configuration file with standard defaults options: '$KERNEL_VERSION'_android has been created..'
 echo 'Please modify the created configuration to enable android modules beeing built within the kernel'
