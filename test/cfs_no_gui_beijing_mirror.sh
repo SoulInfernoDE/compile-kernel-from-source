5f0940c98102c38b1fefe526e6987fbee1a264ad
Soul Inferno DE <SoulInfernoDE@users.noreply.github.com>
2022-06-10 17:04:46 +0200

    script updated to use beijing mirror
    so other people around the world can use the beijing kernel source from code aurora located in china with this small update. Report any issues.

---
+++ test/cfs_no_gui_beijing_mirror.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Save the number of your cores to the variable CPUCORES
+CPUCORES=$(nproc)
+# You can change this variable to compile in any other folder
+CPATH=~/Downloads
+
+echo "Kernel Pull Merge Script v0.1a"
+echo 'Installing dependencies'
+sudo apt install git dwarves build-essential fakeroot bc kmod cpio libxi-dev libncurses5-dev libgtk2.0-dev libglib2.0-dev libglade2-dev libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev dpkg-dev autoconf libdw-dev cmake zstd packagekit qt5ct libpackagekitqt5-dev nano
+cd $CPATH
+# rm linux-*.tar.xz 2> /dev/null
+mkdir kernel # We create a work directory folder
+cd kernel
+read -p "Which kernel version do you want to compile? (example: 5.19-rc1) " KERNEL_VERSION
+echo 'kernel version you entered: '$KERNEL_VERSION'_android'
+git clone https://kernel.source.codeaurora.cn/pub/scm/linux/kernel/git/torvalds/linux.git --depth 1 --branch v$KERNEL_VERSION
+cd linux # we open the kernel source code folder downloaded by git
+cp /boot/config-$(uname -r) ./.config # we copy your current configuration file from /boot/config to the kernel source code folder and rename it to .config
+echo 'Downloading the ASHMEM source code removal patch from upstream'
+wget -O remove_ashmem.patch https://kernel.source.codeaurora.cn/pub/scm/linux/kernel/git/torvalds/linux.git/patch/?id=721412ed3d819e767cac2b06646bf03aa158aaec
+echo ''
+echo 'Reverting the removal patch code..'
+interdiff -q remove_ashmem.patch /dev/null > enable_ashmem.patch
+echo ''
+echo 'Patching the kernel sources to bring back ASHMEM..'
+patch -p1 -i enable_ashmem.patch
+echo ''
+echo 'DONE! ASHMEM is now selectable again in your kernel .config file! NOTE: THIS MAY BREAK ANYTIME AS ASHMEM IS REPLACED WITH MEMFD'
+echo 'which is not supported by Anbox yet..'
+make olddefconfig # we re-generate the copied config file to update new lines in newer kernel versions with the pre-defined default answer
+echo 'Configuration file with standard defaults options: '$KERNEL_VERSION'_android has been created..'
+# Downloading the merge fragments file - if you need this changed create a pull request
+wget https://raw.githubusercontent.com/SoulInfernoDE/compile-kernel-from-source/main/nogui/.config-fragment
+# Using terminal script from torvalds to modify the needed lines into the .config file found in the kernel sources under kernel/scripts/kconfig/merge_config.sh
+# Merge IP fragment CONFIG_ settings into the main .config file
+./scripts/kconfig/merge_config.sh .config .config-fragment
+echo 'merging new android options into the .config file:'
+make olddefconfig # we have added the ANDROID lines at the end of the config file, however we re-generate the config file again to maintain the correct structure
+echo 'You have' $CPUCORES 'cpu cores'
+echo "Ready to start compiling! Enter 'time nice make bindeb-pkg' -j'YOUR NUMBER OF CORES HERE' to start compiling with multi-core mode.."
+echo ''
+read -r -p "
+###############################################################
+# deb-file creation will start. Do you want to continue?      #
+#                                                             #
+#   - Make sure configuration changes are correct!            #
+############################################################### 
+(y|Y)es (n|N)o # " input
+
+case $input in
+    [yY][eE][sS]|[yY])
+ echo "Executing..!"
+ ;;
+    [nN][oO]|[nN])
+ echo "No, we stop here.."
+ exit 1
+       ;;
+    *)
+ echo "Invalid selection input.. ..aborting!"
+ exit 1
+ ;;
+esac
+
+time nice make bindeb-pkg -j'$CPUCORES' # we start compiling process with: counting the time needed to compile, show less and nicer compile information, generate deb-files at the end and use x-cpu cores to speed up compiling procedure
+
+read -r -p "
+###############################################################
+# deb-files will be installed. Do you want to continue?       #
+#                                                             #
+#   - Make sure compiling finished successfully!              #
+############################################################### 
+(y|Y)es (n|N)o # " install
+
+case $install in
+    [yY][eE][sS]|[yY])
+ echo "Executing..!"
+ ;;
+    [nN][oO]|[nN])
+ echo "No, we stop here.."
+ exit 1
+       ;;
+    *)
+ echo "Invalid selection input.. ..aborting!"
+ exit 1
+ ;;
+esac
+
+sudo dpkg -i $CPATH/linux-*.deb # we install the compiled *.deb kernel files
+echo ''
+echo 'If the script has an error for you, please report it on github. You can leave a screenshot if you like to in the issues section'
+echo ''
+echo 'If you are using uefi secure boot within your linux installation then you need to sign your kernel. You can download my script for automation here: Right-click and save-as'
+echo 'https://raw.githubusercontent.com/SoulInfernoDE/compile-kernel-from-source/test/signkernel/cfs_signkernel.sh'
