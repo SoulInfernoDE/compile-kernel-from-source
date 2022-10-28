40b8b723e68d2c762c298c8010d031a927f8df40
Soul Inferno DE <SoulInfernoDE@users.noreply.github.com>
2022-10-29 00:11:49 +0200

    updated mokconfig git address
    updated mokconfig git address due to branch rename/change to 6.x for main

--- signkernel/cfs_signkernel.sh
+++ signkernel/cfs_signkernel.sh
@@ -74,7 +74,7 @@ case $input in
 esac
 
 cd ~
-wget https://raw.githubusercontent.com/SoulInfernoDE/compile-kernel-from-source/main/signkernel/mokconfig.cnf 2> /dev/null
+wget https://raw.githubusercontent.com/SoulInfernoDE/compile-kernel-from-source/v6.x/signkernel/mokconfig.cnf 2> /dev/null
 openssl req -config ./mokconfig.cnf -new -x509 -newkey rsa:2048 -nodes -days 36500 -outform DER -keyout "MOK.priv" -out "MOK.der"
 echo ''
 openssl x509 -in MOK.der -inform DER -outform PEM -out MOK.pem
